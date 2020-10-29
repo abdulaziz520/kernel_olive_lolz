f8d57c7c2d353d9c37ddd5025e6d24b601d94bb8
yi985432 <70029274+yi985432@users.noreply.github.com>
2020-10-29 11:41:20 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,10 @@
+
+sudo apt update && sudo apt -y dist-upgrade
+sudo apt install -y gcc g++ python make texinfo texlive bc bison build-essential ccache curl flex g++-multilib gcc-multilib \
+    git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev \
+    libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
+    unzip language-pack-zh-hans
+
 export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE=aarch64-linux-android-
